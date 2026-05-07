dea57fe67eef6f970adea4d590de9c8beab54774
WebDevWay <k.vasiliy.r@gmail.com>
2026-05-07 14:38:45 +0800

    db

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -35,7 +35,7 @@ class AuthenticatedSessionController extends Controller
     
         session()->flash('success', 'Добро пожаловать! Вы успешно вошли.');
 
-        return redirect()->intended(route('index', absolute: false));
+        return redirect('/');
     }
 
     /**
