8a6d93cefaf6b74caa1f3f6bf99cd00bd64de7ff
WebDevWay <k.vasiliy.r@gmail.com>
2026-05-13 16:09:41 +0800

    last review changes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -60,7 +60,7 @@ class TaskController extends Controller
 
         $task = Auth::user()->createdTasks()->make($validated);
         $task->save();
-
+       
         if (isset($validated['labels']) && $validated['labels'] !== []) {
             $task->labels()->sync($validated['labels']);
         }
@@ -106,9 +106,9 @@ class TaskController extends Controller
         $task->update($validated);
 
         if (isset($validated['labels'])) {
-            $task->labels->sync($validated['labels']);
+            $task->labels()->sync($validated['labels']);
         } else {
-            $task->labels->sync([]);
+            $task->labels()->sync([]);
         }
 
         return redirect()->route('tasks.index')
