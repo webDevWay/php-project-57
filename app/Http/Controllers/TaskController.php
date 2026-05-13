9af78d37b53874143c4755b2ef812ddaf3ca9c7f
WebDevWay <k.vasiliy.r@gmail.com>
2026-05-13 16:49:23 +0800

    last review changes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -121,7 +121,6 @@ class TaskController extends Controller
      */
     public function destroy(User $user, Task $task)
     {
-        dd($task->createdBy->id);
         if ($task->createdBy->id !== Auth::id()) {
             return redirect()->route('tasks.index')
                 ->with('error', ('Невозможно удалить чужую задачу'));
